5bf0268b7af34f21ae9a82360c1bf2ff86d87746
Joel <da1nerd@pm.me>
2024-10-31 15:13:24 +0700

    merged mixin with the remote adapter

--- lib/src/event_remote_adapter.dart
+++ lib/src/event_remote_adapter.dart
@@ -1,6 +1,12 @@
 import 'package:event_sink/event_sink.dart';
 
 abstract class EventRemoteAdapter {
+  /// The pull strategy to apply to the remote events.
+  PullStrategy get pullStrategy;
+
+  /// The priority of the remote adapter.
+  int get priority;
+
   /// Pulls events from the remote source
   /// then apply [pullStrategy].
   Future<List<RemoteEventModel>> pull();
@@ -8,12 +14,4 @@ abstract class EventRemoteAdapter {
   /// Pushes events to the remote source.
   /// Returns the newly created remote events.
   Future<List<RemoteEventModel>> push(List<RemoteNewEventModel> events);
-}
-
-mixin RemoteAdapterProperties {
-  /// The priority of the remote adapter.
-  int get priority;
-
-  /// The pull strategy to apply to the remote events.
-  PullStrategy get pullStrategy;
 }
