1ba87bf7c33bbae7094d863c5d75bcf7459f7ceb
Dhemas Nurjaya <dhemasnurjaya@gmail.com>
2025-01-15 16:20:50 +0700

    export StreamHash

--- lib/event_sink.dart
+++ lib/event_sink.dart
@@ -12,6 +12,7 @@ export 'src/event_params.dart';
 export 'src/event_data.dart';
 export 'src/core/error/failure.dart';
 export 'src/feature/data/local/models/event_model.dart';
+export 'src/feature/data/local/models/stream_hash.dart';
 export 'src/event_remote_adapter.dart';
 export 'src/feature/data/remote/models/remote_event_model.dart';
 export 'src/feature/data/remote/models/remote_new_event_model.dart';
