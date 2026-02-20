4b65c0ce38784a13e9f75a60188db5a13a941205
Dhemas Nurjaya <103254895+dhemasnurjaya@users.noreply.github.com>
2026-02-20 16:06:09 +0700

    update event normalization logic and bump version to 1.6.0 (#38)

--- lib/src/feature/data/remote/data_sources/event_remote_data_source.dart
+++ lib/src/feature/data/remote/data_sources/event_remote_data_source.dart
@@ -66,7 +66,8 @@ class EventRemoteDataSourceImpl implements EventRemoteDataSource {
         }
       }
       // TRICKY: since we send a single event, we only get one back
-      return RemoteEventModel.fromJson(json.decode(response.body)["events"][0]);
+      return RemoteEventModel.fromJson(
+          _normalizeEventJson(json.decode(response.body)["events"][0]));
     } on ServerException {
       rethrow;
     } catch (e, trace) {
@@ -94,8 +95,9 @@ class EventRemoteDataSourceImpl implements EventRemoteDataSource {
       }
       final responseJson = json.decode(response.body);
       final List<dynamic> events = responseJson['events'] as List<dynamic>;
-      final remoteEvents =
-          events.map((e) => RemoteEventModel.fromJson(e)).toList();
+      final remoteEvents = events
+          .map((e) => RemoteEventModel.fromJson(_normalizeEventJson(e)))
+          .toList();
       remoteEvents.sort((a, b) => a.order.compareTo(b.order));
       return remoteEvents;
     } on ServerException {
@@ -103,6 +105,17 @@ class EventRemoteDataSourceImpl implements EventRemoteDataSource {
     } catch (e, trace) {
       throw ServerException(message: '$e\n\n$trace');
     }
+  }
+
+  // Normalizes an event JSON map, handling the case where [data] is a
+  // JSON-encoded string instead of an embedded JSON object.
+  Map<String, dynamic> _normalizeEventJson(dynamic e) {
+    final map = e as Map<String, dynamic>;
+    final data = map['data'];
+    if (data is String) {
+      return {...map, 'data': json.decode(data) as Map<String, dynamic>};
+    }
+    return map;
   }
 
   // Checks if the response contains a version error
